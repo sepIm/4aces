fd9cffdb4bc25f17b59c5574bb38acdb35e2e75d
SepIm <s.parodi@ligurtek.it>
2021-10-14 16:50:26 +0200

    calcolatrice import di 2 numeri

---
+++ src/Calc4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Calc4 {
+    public static void main(){
+        System.out.println("Benvenuto nella calcolatrice dei Fantastici 4");
+        Scanner inserisci = new Scanner(System.in);
+        System.out.println("Digita il primo numero");
+        int numeroA=inserisci.nextInt();
+        System.out.println("Digita il seconda numero");
+        int numeroB=inserisci.nextInt();
+    }
+}
